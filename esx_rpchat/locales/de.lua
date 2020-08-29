a8109aa15c87296f5a7cbecc1a301d77ad6ef238
sM00thCF <61839701+sM00thCF@users.noreply.github.com>
2020-08-29 17:30:11 +0200

    Create de.lua

---
+++ esx_rpchat/locales/de.lua
@@ -0,0 +1,12 @@
+Locales['de'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'Tweet senden',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'persönliches Handeln',
+  ['me_prefix'] = 'ich | %s',
+  ['do_help'] = 'RP Informationen',
+  ['do_prefix'] = 'do | %s',
+  ['generic_argument_name'] = 'Nachricht',
+  ['generic_argument_help'] = 'Die Nachrichte',
+  ['unknown_command'] = '^3%s^0 ist kein valider Befehl!',
+}
